68ff6e7123a40aa51a36f83b19830454afb544a8
h4rlen <harlengiraldo@seven4n.com>
2018-06-02 19:28:11 -0500

    se actualiza interfaz

--- app.js
+++ app.js
@@ -1,127 +1,199 @@
-(function () {
-    'use strict';
-    var initChiste = {
-        key: '0',
-        mensaje: 'cargando chiste.',
-        icon_url: './imagenes/hellscream.gif',
-    };
-    var app = {
-        isLoading: true,
-        chiste: {},
-        spinner: document.querySelector('.loader__spinner'),
-        container: document.querySelector('.vertical-container'),
+(function() {
+  'use strict';
+  var initChiste = {
+    key: '0',
+    mensaje: 'cargando chiste.',
+    icon_url: './imagenes/hellscream.gif'
+  };
+  var initUsuario = {
+    cell: '3XXXXXXX',
+    nombre: 'XXXXXX',
+    apellido: 'XXXXXXX',
+    titulo: 'XX',
+    phone: '2XXXXXX',
+    imagenGrande: './imagenes/hellscream.gif'
+  };
+  var app = {
+    isLoading: true,
+    chiste: {},
+    usuario: {},
+    spinner: document.querySelector('.loader__spinner'),
+    container: document.querySelector('.vertical-container')
+  };
+  // ACCIONES DEL USUARIO
+  document.getElementById('butRefresh').addEventListener('click', function() {
+    // Refresh contendio
+    app.actualizarApp();
+  });
 
-    };
-    // ACCIONES DEL USUARIO
-    document.getElementById('butRefresh').addEventListener('click', function () {
-        // Refresh contendio
-        app.actualizarChiste();
-    });
-
-    // FUNCIONES
-    app.obtenerChiste = function () {
-        var url = 'https://api.chucknorris.io/jokes/random';
-        // TODO add cache logic here
-        if ('caches' in window) {
-            /*
+  // FUNCIONES
+  app.obtenerChiste = function() {
+    var url = 'https://api.chucknorris.io/jokes/random';
+    // TODO add cache logic here
+    if ('caches' in window) {
+      /*
              * MIRA si el servidor ya ha cacheado un chiste
              * Si el servidor ha cacheado la informacion la muestra
              * Mientras actualiza la data.
              */
-            caches.match(url).then(function (response) {
-                if (response) {
-                    response.json().then(function updateFromCache(json) {
-                        var results = json;
-                        results['key'] = json.id;
-                        results['mensaje'] = json.value;
-                        results['icon_url'] = json.icon_url;
-                        app.actualizarChisteDom(results);
-                    });
-                }
-            });
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {
+            var results = json;
+            results['key'] = json.id;
+            results['mensaje'] = json.value;
+            results['icon_url'] = json.icon_url;
+            app.actualizarChisteDom(results);
+          });
         }
-        // Fetch the latest data.
-        mostrarSpinner();
+      });
+    }
+    // Fetch the latest data.
+    mostrarSpinner();
 
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState === XMLHttpRequest.DONE) {
-                if (request.status === 200) {
-                    var response = JSON.parse(request.response);
-                    var results = response;
-                    results['key'] = response.id;
-                    results['mensaje'] = response.value;
-                    results['icon_url'] = response.icon_url;
-                    app.actualizarChisteDom(results);
-                    app.chiste = results;
-                    app.guardarChiste();
-
-                }
-            } else {
-                // En caso bad, retorna el estado inicial.
-                app.actualizarChisteDom(initChiste);
-            }
-        };
-        request.open('GET', url);
-        request.send();
-    };
-
-    app.actualizarChisteDom = function (data) {
-        document.querySelector('.id').textContent = data.id;
-        document.querySelector('.mensaje').textContent = data.value;
-        document.querySelector('.imagen').src = data.icon_url;
-        if (app.isLoading) {
-            app.spinner.setAttribute('hidden', true);
-            app.container.removeAttribute('hidden');
-            app.isLoading = false;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          var results = response;
+          results['key'] = response.id;
+          results['mensaje'] = response.value;
+          results['icon_url'] = response.icon_url;
+          app.actualizarChisteDom(results);
+          app.chiste = results;
+          app.guardarChiste();
         }
-    }
-    app.actualizarChiste = function () {
-        mostrarSpinner();
-        app.obtenerChiste();
-    }
-    function mostrarSpinner() {
-        app.isLoading = true;
-        app.spinner.removeAttribute('hidden');
-
-    }
-    // Guarda chistes en localStorage.
-    app.guardarChiste = function () {
-        localStorage.removeItem('chiste');
-        var chiste = JSON.stringify(app.chiste);
-        localStorage.chiste = chiste;
+      } else {
+        // En caso bad, retorna el estado inicial.
+        app.actualizarChisteDom(initChiste);
+      }
     };
+    request.open('GET', url);
+    request.send();
+  };
+  app.obtenerUsuarioRamdom = function() {
+    var url = 'https://randomuser.me/api/';
+    // TODO add cache logic here
+    if ('caches' in window) {
+      /*
+             * MIRA si el servidor ya ha cacheado un usuario
+             * Si el servidor ha cacheado la informacion la muestra
+             * Mientras actualiza los datos.
+             */
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {
+            var results = json;
+            var results = {};
+            results['cell'] = json.cell;
+            results['nombre'] = json.nombre;
+            results['apellido'] = json.apellido;
+            results['titulo'] = json.titulo;
+            results['phone'] = json.phone;
+            results['imagenGrande'] = json.imagenGrande;
+            app.actualizarUserDom(results);
+          });
+        }
+      });
+    }
+    // Fetch the latest data.
+    mostrarSpinner();
 
-    /*****************************************************************************
-     *
-     * Y ASI INICIA TODO
-     *
-     ****************************************************************************/
-    app.chiste = localStorage.chiste;
-    if (app.chiste) {
-        app.obtenerChiste();
-        console.log('OBTENGA COSAS OME');
-    } else {
-        /* Si el usuario usa la app por primera vez, o el uusuario no a guardado chistes
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          var response = JSON.parse(request.response);
+          var results = {};
+          results['cell'] = response.results[0].cell;
+          results['correo'] = response.results[0].email;
+          results['nombre'] = response.results[0].name.first;
+          results['apellido'] = response.results[0].name.last;
+          results['titulo'] = response.results[0].name.title;
+          results['phone'] = response.results[0].phone;
+          results['imagenGrande'] = response.results[0].picture.large;
+          app.actualizarUserDom(results);
+        }
+      } else {
+        app.actualizarUserDom(initUsuario);
+
+        // En caso bad, retorna el estado inicial.
+      }
+    };
+    request.open('GET', url);
+    request.send();
+  };
+  app.actualizarChisteDom = function(data) {
+    document.querySelector('.mensaje').textContent = data.value;
+    if (app.isLoading) {
+      app.container.removeAttribute('hidden');
+      app.isLoading = false;
+    }
+  };
+  app.actualizarUserDom = function(data) {
+    document.querySelector('#correo').textContent = data.correo;
+    document.querySelector('#cel').textContent = data.cell;
+    document.querySelector('#tel').textContent = data.phone;
+    document.querySelector('.user-img-grande').src = data.imagenGrande;
+    document.querySelector('#titulo').textContent = data.titulo;
+    document.querySelector('.user').textContent = data.nombre;
+    document.querySelector('#nombre').textContent =
+      data.nombre + ' ' + data.apellido;
+    if (app.isLoading) {
+      app.container.removeAttribute('hidden');
+      app.isLoading = false;
+    }
+  };
+  app.actualizarApp = function() {
+    mostrarSpinner();
+    app.obtenerChiste();
+    app.obtenerUsuarioRamdom();
+  };
+  function mostrarSpinner() {
+    app.isLoading = true;
+  }
+  // Guarda chistes en localStorage.
+  app.guardarChiste = function() {
+    localStorage.removeItem('chiste');
+    var chiste = JSON.stringify(app.chiste);
+    localStorage.chiste = chiste;
+  };
+
+  /*****************************************************************************
+   *
+   * Y ASI INICIA TODO
+   *
+   ****************************************************************************/
+  app.chiste = localStorage.chiste;
+  if (app.chiste) {
+    app.obtenerChiste();
+    app.obtenerUsuarioRamdom();
+    console.log('OBTENGA COSAS OME');
+  } else {
+    /* Si el usuario usa la app por primera vez, o el uusuario no a guardado chistes
         * Entonces muestre chiste temporal. 
         */
-        app.actualizarChisteDom(initChiste);
-        app.chiste = initChiste;
-        app.guardarChiste();
-        console.log('SETEO COSAS  COSAS OME');
-    }
-    // Preguntamos al usuario si el navegador soporta Services Workers
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-        console.log('Service Worker y Push notificaciones son soportados ');
-        // Registramos el service Worker
-        navigator.serviceWorker.register('/sw.js')
-            .then(function (swReg) {
-                console.log('Service Worker Registrado');
-            }).catch(function (error) {
-                console.error('[Service Worker Error]:', error);
-            });
-    } else {
-        console.warn('Las notificaciones push no esta soportado');
-    }
-
+    app.actualizarChisteDom(initChiste);
+    app.actualizarUserDom(initUsuario);
+    app.chiste = initChiste;
+    app.usuario = initUsuario;
+    app.guardarChiste();
+    console.log('SETEO COSAS  COSAS OME');
+  }
+  // Preguntamos al usuario si el navegador soporta Services Workers
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker y Push notificaciones son soportados ');
+    // Registramos el service Worker
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function(swReg) {
+        console.log('Service Worker Registrado');
+      })
+      .catch(function(error) {
+        console.error('[Service Worker Error]:', error);
+      });
+  } else {
+    console.warn('Las notificaciones push no esta soportado');
+  }
 })();
--- sw.js
+++ sw.js
@@ -7,34 +7,35 @@ var filesToCache = [
   '/imagenes/giphy.gif'
 ];
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        console.log('[asdasdasdad]',key);
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          console.log('[asdasdasdad]', key);
+          if (key !== cacheName && key !== dataCacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
   return self.clients.claim();
-
 });
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
   var dataUrl = 'https://api.chucknorris.io/jokes/random';
   if (e.request.url.indexOf(dataUrl) > -1) {
@@ -46,8 +47,8 @@ self.addEventListener('fetch', function (e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
      */
     e.respondWith(
-      caches.open(dataCacheName).then(function (cache) {
-        return fetch(e.request).then(function (response) {
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response) {
           cache.put(e.request.url, response.clone());
           return response;
         });
@@ -60,21 +61,21 @@ self.addEventListener('fetch', function (e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
      */
     e.respondWith(
-      caches.match(e.request).then(function (response) {
+      caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
       })
     );
   }
 });
 // Notificacion cuando cierra
-self.addEventListener('notificationclose', function (e) {
+self.addEventListener('notificationclose', function(e) {
   var notification = e.notification;
   var primaryKey = notification.data;
 
   console.log('Closed notification: ' + primaryKey);
 });
-// Notificacion al hacer click 
-self.addEventListener('notificationclick', function (e) {
+// Notificacion al hacer click
+self.addEventListener('notificationclick', function(e) {
   var notification = e.notification;
   var primaryKey = notification.data.primaryKey;
   var action = e.action;
